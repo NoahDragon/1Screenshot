77c14f6ac470e7e1a6d16e4b99186d6590cfac4c
Abner Chou <zhcjtht@hotmail.com>
2017-02-08 15:54:09 -0500

    Experiment

--- renderer.js
+++ renderer.js
@@ -1,3 +1,12 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// My code
+ReactDOM.render(
+        React.DOM.h1({id: "First H1"}, React.DOM.span(null, React.DOM.em(null,"Hello World!"), "My first Reactjs!")),
+        document.getElementById("app")
+);
